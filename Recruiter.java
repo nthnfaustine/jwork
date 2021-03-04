d372ed97fa4baca72f06a492abd882d34f36566a
nathan <faustinenathaniel@gmail.com>
2021-03-04 17:02:29 +0700

    modul 1

---
+++ Recruiter.java
@@ -0,0 +1,47 @@
+public class Recruiter
+{
+    // instance variables - replace the example below with your own
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String joinDate;
+
+    public Recruiter(){
+        // initialise instance variables
+    }
+
+    public static int getId(){
+        return 0;
+    }
+    
+    public static String getName(){
+        return "oop";
+    }
+    
+        
+    public static String getEmail(){
+        return "oop";
+    }
+    
+    public static String getPhoneNumber(){
+        return "oop";
+    }
+    
+    public static void setId(int id){
+    
+    }
+    
+    public static void setEmail(String Email){
+        
+    }
+    
+    public static void setName(String Name){
+    
+    }
+    
+    public static void setPhoneNumber(String phoneNumber){
+        
+    }
+}
+
